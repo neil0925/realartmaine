4d17f0c7089169b31b50e7b403a991e50a067e0e
neil0925 <neilfrogmam@gmail.com>
2025-10-26 17:05:01 -0400

    Fix image paths in gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -7,9 +7,9 @@ const imagesList = [
 "/images/5G-TroubledGuppy-piece.jpg",
 "/images/aura-OHK-troubledguppy-piece.jpg",
 "/images/blame-troubledguppy-piece.jpg",
-"/images/brik-PTG-troubledguppy-piece",
-"/images/burek, house-2GF,CTS,TNL-troubledgubby-piece",
-"/images/burek-2GF-troubledguppy-piece",
+"/images/brik-PTG-troubledguppy-piece.jpg",
+"/images/burek, house-2GF,CTS,TNL-troubledgubby-piece.jpg",
+"/images/burek-2GF-troubledguppy-piece.jpg",
 ];
 
 const gallery = document.getElementById('galleryContainer');
