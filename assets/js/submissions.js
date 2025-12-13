a7ff1351221054b81cadd0a8fce23d6e992807b8
neil0925 <neilfrogmam@gmail.com>
2025-12-13 15:24:22 -0500

    update

--- assets/js/submissions.js
+++ assets/js/submissions.js
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
     html += '<div class="table-wrap"><table class="leaderboard-table"><thead><tr>' +
       '<th class="rank-col">Place</th>' +
       '<th class="photog-col">Photographer</th>' +
-      '<th class="count-col">Number of Submissions</th>' +
+      '<th class="count-col">Submissions</th>' +
       '</tr></thead><tbody>';
 
     list.slice(0, 200).forEach(([name, cnt], idx) => {
